1bc5967fecdc42f138822cac5c37dba28a566693
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2020-01-21 14:20:47 +0000

    source cmd update

--- commandes/source.js
+++ commandes/source.js
@@ -1,13 +1,15 @@
 const log = require("./utils/log.js");
 
 module.exports.run = function sourceCmd(client, guild, user, args, answer_channel) {
-  answer_channel.send("https://repl.it/@AnicetN/error-4-104-bot");
+  let str = "sur Repl: https://repl.it/@AnicetN/error-4-104-bot";
+  str += "\nsur GitHub: https://github.com/AnicetNgrt/error-4-104-bot";
+  answer_channel.send(str);
 };
 
 module.exports.meta = {
   name: "source",
   help: {
-    desc: "Donne le lien vers le code source du bot",
+    desc: "Donne les liens vers le code source du bot",
     args: []
   },
   argCount: 0
